d5555d392eae36809b3462090ae323b18c340146
gaboulanger <gaboulanger@btslivh.eu>
2022-06-16 11:28:43 +0200

    Ajout d'icones GAB

---
+++ ceseat/src/views/LogInView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="logIn">
+      <img alt="CeseatLogo" src="../assets/logo.png">
+      <HelloWorld msg="Ceci est la page de connexion de ceseat"/>
+    <h1>Ceci est la page de connexion de ceseat</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class HomeView extends Vue {}
+</script>
