3e6edc9f5cfa449d91b8d8c035a8b442fff9d54b
margauxclaret <margaux.claret@viacesi.fr>
2022-06-21 11:14:14 +0200

    changement repertoire

--- ceseat/src/views/AboutView.vue
+++ ceseat/src/views/AboutView.vue
@@ -1,8 +1,36 @@
-<template>
+<!--<template>
   <div class="about">
     <h1>This is an about page</h1>
     <v-btn
   elevation="2"
 ></v-btn>
   </div>
+</template>-->
+<!-- Vue Template HTML Markup -->
+
+<template>
+  <v-menu>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        v-bind="attrs"
+        v-on="on"
+      >
+        Click me
+      </v-btn>
+    </template>
+
+    <v-list>
+      <v-list-item @click="method">
+        <v-list-item-title>Option 1</v-list-item-title>
+      </v-list-item>
+
+      <v-list-item disabled>
+        <v-list-item-title>Option 2</v-list-item-title>
+      </v-list-item>
+
+      <v-list-item @click="method">
+        <v-list-item-title>Option 3</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
 </template>
