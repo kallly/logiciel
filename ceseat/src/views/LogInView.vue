ea86a645801102fa5a210f181a3485d5f1226440
gaboulanger <gaboulanger@btslivh.eu>
2022-06-21 10:29:43 +0200

    commentatisation err fichier login.vue

--- ceseat/src/views/LogInView.vue
+++ ceseat/src/views/LogInView.vue
@@ -19,11 +19,11 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+/*
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {}*/
 </script>
