2cb50a9adf606b73184769565d2c8641726c3359
gaboulanger <gaboulanger@btslivh.eu>
2022-06-21 11:21:36 +0200

    navigation sur vuetify

--- ceseat/src/App.vue
+++ ceseat/src/App.vue
@@ -1,40 +1,27 @@
 <template>
-  <div id="app">
-      <v-toolbar color="purple" elevation="24"></v-toolbar>
-    <nav>
+  <v-app>
+    <v-app-bar>
+     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/login">logIn</router-link>
     </nav>
-    
-    <router-view />
-    
+    </v-app-bar>
 
-  </div>
-
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
 
+export default Vue.extend({
+  name: 'App',
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+});
+</script>
