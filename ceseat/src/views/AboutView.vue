3843445b6cd751c664681ad1c94d732fc65217e0
margauxclaret <margaux.claret@viacesi.fr>
2022-06-28 10:22:33 +0200

    hello word delete

--- ceseat/src/views/AboutView.vue
+++ ceseat/src/views/AboutView.vue
@@ -9,7 +9,6 @@
   import Vue from 'vue'
   import ResizerComponent from '../components/Responsive/ResizerComponent.vue'
   import TopbarComponent from '../components/Navigation/TopbarComponent.vue'
-  import HelloWorld from '../components/HelloWorld.vue'
   export default Vue.extend({
     name: 'about',
     components: {
